e941cb78d0e6d6fbc725c7ad1dd0e1b16b8ac77c
sandeep <jadhavsandeep1189@gmail.com>
2025-04-24 00:29:58 -0400

    tfvar created

--- Day-3/variables.tf
+++ Day-3/variables.tf
@@ -1,11 +1,18 @@
 variable "ami" {
   description = "AMI ID for the instance"
   type        = string
-  default = "ami-00a929b66ed6e0de6"  
+  default = ""  
   
 }
 variable "instance_type" {
   description = "Type of instance to create"
   type        = string
-  default     = "t2.micro"
+  default     = ""
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+  default     = ""
+  
 }
