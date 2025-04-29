08c0460793cd94de0b4dc130e11423842ca47b7c
sandeep <jadhavsandeep1189@gmail.com>
2025-04-28 20:39:21 -0400

    day-4withstatefilelocking

---
+++ Day-4-statefile-remotelocking/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "sj-terraform-statefile-bucket"
+    key            = "statefile/terraform.tfstate"
+    region         = "us-east-1"
+    # dynamodb_table = "terraform-lock-table"
+    use_lockfile = true
+    encrypt        = true
+  }
+}
